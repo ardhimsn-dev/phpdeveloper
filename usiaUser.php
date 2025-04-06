e9a5647d89499849a7e798d69c11a92064540fca
MUHAMMAD SUFI NARITSUL ARDHI <36657204+ardhimsn-dev@users.noreply.github.com>
2025-04-06 17:37:40 +0800

    Update usiaUser.php
    Update Problem Solved (06.04.2025)

--- usiaUser.php
+++ usiaUser.php
@@ -3,13 +3,32 @@
 $tgl_lahir = "15-01-1992";
 $tgl_sekarang = date("d-m-Y");
 
-mysql_connect("localhost:8080","root","");
-mysql_select_db("dbtrik");
+$tgl_lahir_mysql = date("Y-m-d", strtotime($tgl_lahir));
+$tgl_sekarang_mysql = date("Y-m-d", strtotime($tgl_sekarang));
 
-$query = "SELECT date_diff('$tgl_sekarang', '$tgl_lahir') as selisih";
-$hasil = mysql_query($query);
-$data = mysql_fetch_array($hasil);
+// Koneksi ke MySQL dengan mysqli
+$koneksi = mysqli_connect("localhost", "root", "", "dbtrik");
 
-echo "Tanggal Lahirnya : $tgl_lahir<br>;
-Sekarang Usianya : $data[selisih] hari";
+// Cek koneksi
+if (!$koneksi) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+
+// Query untuk menghitung selisih hari antara tanggal lahir dan tanggal sekarang
+$query = "SELECT DATEDIFF('$tgl_sekarang_mysql', '$tgl_lahir_mysql') AS selisih";
+$hasil = mysqli_query($koneksi, $query);
+
+// Cek hasil query
+if (!$hasil) {
+    die("Query gagal: " . mysqli_error($koneksi));
+}
+
+// Ambil hasil dan tampilkan
+$data = mysqli_fetch_array($hasil);
+
+echo "Tanggal Lahirnya: $tgl_lahir<br>";
+echo "Sekarang Usianya: $data[selisih] hari";
+
+// Menutup koneksi setelah selesai
+mysqli_close($koneksi);
 ?>
